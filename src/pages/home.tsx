28429897d9bc75f26b2ef0d4d0906a74da72bd91
folli14 <cesarfolli@gmail.com>
2024-10-11 21:04:20 -0300

    adicionando arquivo home

---
+++ src/pages/home.tsx
@@ -0,0 +1,8 @@
+const home: React.FC = () => {
+    return (
+        <div className="flex justify-center items-center">
+            <h1 className="text-5xl font-bold">Observatório Digital de Discurso e Direitos Humanos</h1>
+        </div>
+    )
+}
+export default home;
