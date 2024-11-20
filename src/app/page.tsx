0f77235463adde77b34ae3edf3b5ae62f8d92678
follijulio <follijulio@gmail.com>
2024-11-20 18:31:56 -0300

    adição do conteúdo do page

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+const page: React.FC = () => {
+    return (
+        <div className="h-screen w-screen flex items-center justify-center bg-slate-700">
+            <h1 className="text-2xl text-white font-extrabold">
+                ObservaDH
+            </h1>
+        </div>
+    )
+}
+
+export default page;
