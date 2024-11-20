e8e65cff2f70853510ef7a98fe1dc7b8c9dc30d6
follijulio <follijulio@gmail.com>
2024-11-20 18:15:50 -0300

    remoção do arquivo home (arquivo desnecessário)

--- src/pages/home.tsx
+++
@@ -1,8 +0,0 @@
-const home: React.FC = () => {
-    return (
-        <div className="flex justify-center items-center">
-            <h1 className="text-5xl font-bold">Observatório Digital de Discurso e Direitos Humanos</h1>
-        </div>
-    )
-}
-export default home;
