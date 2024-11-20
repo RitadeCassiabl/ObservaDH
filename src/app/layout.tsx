ae535862699e464d0afb481f1e253a081675b5d9
follijulio <follijulio@gmail.com>
2024-11-20 18:30:27 -0300

    layout configurado

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import "./globals.css";
+export default function RootLayout({
+  children
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="pt-br">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
