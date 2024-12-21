542b03decf2d53211107595c5ab2b309a25cf380
follijulio <follijulio@gmail.com>
2024-12-20 22:45:56 -0300

    remoção do conteúdo da page principal para testes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 const page: React.FC = () => {
     return (
-        <div className="h-screen w-screen flex items-center justify-center bg-slate-700">
-            <h1 className="text-2xl text-white font-extrabold">
-                ObservaDH
-            </h1>
+        <div>
+            
         </div>
     )
 }
