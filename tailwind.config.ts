7f17939a07dca35929c98b407e2a89eff79d654c
follijulio <follijulio@gmail.com>
2024-12-20 22:17:06 -0300

    adição de configuração estilizada do shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       backgroundImage: {
         'layout-principal': 'linear-gradient(135deg, #060C18 0%, #122144 49%, #1A326E 100%)',
       },
+      boxShadow: {
+        'bottom': '0 1px 10px 1px rgba(0, 0, 0, 0.1), 0 0 0 0 0  rgba(0, 0, 0, 0)',
+      },
     },
   },
   plugins: [],
