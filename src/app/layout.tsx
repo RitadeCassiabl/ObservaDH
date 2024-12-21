61e9da0f09f4f71e26b953abfc960fbe4b0fd9cd
follijulio <follijulio@gmail.com>
2024-12-20 22:45:33 -0300

    adição do layout principal ao layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,17 @@ export const metadata: Metadata = {
     title: "ObservaDH",
     description: "Observatório de Projetos de Lei voltados aos Direitos LGBTI+.",
 };  
+
 export default function RootLayout({
   children
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-br" className={titillium_web.className}>
+      <body className="h-screen w-screen bg-layout-principal antialiased p-8 flex justify-center">
+        <NavBar />
+        <div>
+          {children}
+        </div>
       </body>
     </html>
   );
