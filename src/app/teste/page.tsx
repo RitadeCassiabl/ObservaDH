8c2ae2467191ab413f0b4f1f5229e60682ef2ca7
follijulio <follijulio@gmail.com>
2024-12-26 13:16:40 -0300

    criando página de testes dos gráficos

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,8 @@
+const page: React.FC = () => {
+    return (
+        <div className="h-screen w-screen flex items-center justify-center bg-slate-700">
+            PÁGINA DE TESTES      
+        </div>
+    );
+}
+export default page;
