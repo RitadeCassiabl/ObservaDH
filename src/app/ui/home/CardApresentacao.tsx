1adeec250bcf23706afdd7bf18ba158faea6bdbb
follijulio <follijulio@gmail.com>
2024-12-26 20:24:28 -0300

    fix: Fonte do Card inicial da home

--- src/app/ui/home/CardApresentacao.tsx
+++ src/app/ui/home/CardApresentacao.tsx
@@ -12,9 +12,7 @@ interface CardApresentacaoProps {
   children?: React.ReactNode;
 }
 
-const cardApresetacao: React.FC<CardApresentacaoProps> = ({
-children
-}) => {
+const cardApresetacao: React.FC<CardApresentacaoProps> = ({ children }) => {
   return (
     <section className="w-[75.75rem] h-[43.4375rem] bg-gradient-to-b from-[#050B17] to-[#1A326E]  border-[1px] border-[#87D9FF] p-16 rounded-[5px] shadow-lg shadow-[#87D9FF]">
       <article className="w-full h-full flex flex-col gap-4">
@@ -23,15 +21,15 @@ children
         >
           O que é o{" "}
           <span
-            className={`text-[#87D9FF] ${titillium_web.className} font-light`}
+            className={`text-[#87D9FF] ${titillium_web.className} font-extralight`}
           >
             ObservaDH?
           </span>
         </h2>
         <section className="h-[28rem] w-full overflow-y-auto shadow-inner no-scrollbar">
           <div className="text-white text-2xl font-light text-justify ">
-           {children}
-           </div>
+            {children}
+          </div>
         </section>
       </article>
     </section>
