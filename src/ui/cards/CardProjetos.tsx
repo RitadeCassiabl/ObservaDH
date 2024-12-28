02fc9125b60116172b859d7d10b3d43442c4c78b
follijulio <follijulio@gmail.com>
2024-12-28 15:56:23 -0300

    adição do componente CardProjetos para exibir informações de projetos de lei

---
+++ src/ui/cards/CardProjetos.tsx
@@ -0,0 +1,61 @@
+import { Card } from "@/components/ui/card";
+import { ProjetoLei } from "@/lib/types/projetos";
+import { Titillium_Web, Oswald } from "next/font/google";
+import React from "react";
+
+const oswald = Oswald({ weight: ["400", "700"], subsets: ["latin"] });
+
+const titillium_web = Titillium_Web({
+  weight: ["400", "700"],
+  subsets: ["latin"]
+});
+
+interface CardProjetosProps {
+  projeto: ProjetoLei;
+}
+
+const CardProjetos: React.FC<CardProjetosProps> = ({ projeto }) => {
+  return (
+    <Card className="bg-[#1A326E] h-[27.625rem] w-[38.125rem] border-[#AFC4F9] border-2 rounded-[10px] px-8 py-12">
+        <RenderText conteudo={projeto.ano} titulo="Ano" />
+        <RenderText conteudo={projeto.numero_pl} titulo="Número do PL" />
+        <RenderText conteudo={projeto.pauta} titulo="Pauta" />
+        <RenderText conteudo="" titulo="" />
+        <RenderText conteudo="" titulo="" />
+
+        
+      {projeto.parlamentares.length > 1
+        ? <div>
+            <h3>Parlamentares:</h3>
+            {projeto.parlamentares.map((parlamentar, index) =>
+              <div key={index} className="text-white">
+                {parlamentar.nome} - {parlamentar.partido}
+              </div>
+            )}
+          </div>
+        : <div>
+            <h3>Parlamentar:</h3>
+            {projeto.parlamentares.map((parlamentar, index) =>
+              <div key={index} className="text-white">
+                {parlamentar.nome} - {parlamentar.partido}
+              </div>
+            )}
+          </div>}
+    </Card>
+  );
+};
+
+interface RenderTextProps {
+  titulo: string;
+  conteudo: string;
+}
+
+const RenderText: React.FC<RenderTextProps> = ({ titulo, conteudo }) => {
+  return (
+    <h3 className="text-white">
+      <span className="font-bold">{titulo}:</span> {conteudo}
+    </h3>
+  );
+};
+
+export default CardProjetos;
