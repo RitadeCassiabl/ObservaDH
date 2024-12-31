604f7c9537e9364092c3cf546323e99dc8a37aff
follijulio <follijulio@gmail.com>
2024-12-31 13:31:28 -0300

    adiciona filtros com DropdownButtons e exibe gráficos de linha com LineChartDots

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,6 +1,9 @@
-import CardApresentacao from "../../ui/cards/CardApresentacao";
-import { apresentacao, projetosMock } from "../../lib/mock/mock_projetos";
-import CardGraficoMapa from "@/ui/cards/CardGraficoMapa";
+import {
+  apresentacao,
+  projetosMock,
+  mockData
+} from "../../lib/mock/mock_projetos";
+
 import {
   Carousel,
   CarouselContent,
@@ -8,12 +11,38 @@ import {
   CarouselNext,
   CarouselPrevious
 } from "@/components/ui/carousel";
+import { LineChartDots } from "@/ui/graficos/LineChartDots";
+
+import Legenda from "@/ui/Legenda";
+import DropdownButton from "@/ui/dropdown/DropdownButton";
+import CardGraficoMapa from "@/ui/graficos/MapaGrafico";
 import CardProjetos from "@/ui/cards/CardProjetos";
+import CardApresentacao from "@/ui/cards/CardApresentacao";
+
+import { Titillium_Web, Oswald } from "next/font/google";
+
+const oswald = Oswald({ weight: ["400", "700"], subsets: ["latin"] });
+
+const titillium_web = Titillium_Web({
+  weight: ["400", "700"],
+  subsets: ["latin"]
+});
+
+const esferas = [
+  {
+    titulo: "Federal",
+    value: "federal"
+  },
+  {
+    titulo: "Estadual",
+    value: "estadual 1"
+  }
+];
 
 const projetos: React.FC = () => {
   return (
-    <div className="flex h-full w-full flex-col gap-[4.25rem] py-[4.375rem] items-center">
-      <div>
+    <div className="flex h-full w-full flex-col gap-[4.25rem]  items-center">
+      <section>
         <CardApresentacao
           subtitulo={apresentacao.subtitulo}
           titulo={apresentacao.titulo}
@@ -21,33 +50,63 @@ const projetos: React.FC = () => {
         >
           {apresentacao.texto}
         </CardApresentacao>
-      </div>
-      <div className="flex flex-col items-center w-full h-[45.625rem] gap-20">
-        <div className="w-[80rem] h-full">
+      </section>
+      <article className="flex flex-col items-center w-full  gap-20 bg-">
+        <section className="w-[80rem] h-full">
           <CardGraficoMapa />
-        </div>
-        <span className="border-b-[1.5px] shadow-bottom shadow-[#AFC4F9] w-[82rem]" />
+        </section>
+        <span className="border-b-[1.5px] shadow-bottom shadow-[#AFC4F9] w-full" />
+        {/* FILTROS */}
+        <section className="text-7xl text-white text-shadow-xl">
+          <span className={`${titillium_web.className} font-light`}>
+            Propostas{" "}
+          </span>
+          <span className={`${oswald.className} font-normal`}>
+            e Dados Estatísticos
+          </span>
+        </section>
+        {/* FILTROS - DROPDOWN*/}
+        <section className="w-full flex gap-12 px-10">
+          <DropdownButton
+            elementos={esferas}
+            titulo="Esfera"
+            className="w-32"
+          />
+          <DropdownButton elementos={esferas} titulo="Ano" className="w-32" />
+          <DropdownButton
+            elementos={esferas}
+            titulo="Estado"
+            className="w-32"
+          />
+          <DropdownButton elementos={esferas} titulo="Pauta" className="w-32" />
+        </section>
         {/* CARROSSEL DE PL's */}
-        <Carousel
-          opts={{
-            align: "start"
-          }}
-          className="w-[82rem]"
-        >
-          <CarouselContent className="">
-            {projetosMock.map((item , index) =>
-              <CarouselItem
-                key={index}
-                className="basis-1/2 flex justify-center"
-              >
-                <CardProjetos projeto={item}/>
-              </CarouselItem>
-            )}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-      </div>
+        <section>
+          <Carousel
+            opts={{
+              align: "start"
+            }}
+            className="w-[82rem]"
+          >
+            <CarouselContent className="">
+              {projetosMock.map((item, index) =>
+                <CarouselItem
+                  key={index}
+                  className="basis-1/2 flex justify-center"
+                >
+                  <CardProjetos projeto={item} />
+                </CarouselItem>
+              )}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </section>
+        <section className="w-full flex justify-center' gap-[4.5rem]">
+          <Legenda />
+          <LineChartDots data={mockData} />
+        </section>
+      </article>
 
       <div className="h-[45.625rem] w-full p-8" />
       <div className="h-[45.625rem] w-full p-8" />
