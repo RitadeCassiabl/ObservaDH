c3e12f96fb6dc6e46096176d8b551acfc702808d
follijulio <follijulio@gmail.com>
2025-01-04 07:04:53 -0300

    adiciona tipos para gráficos de barra empilhada e linha de pontos

---
+++ src/lib/types/graficos.ts
@@ -0,0 +1,12 @@
+export type DadosGraficoBarraEmpilhada = {
+    ano: string
+    linguagensNeutra: number
+    atletasTrans: number
+    banheirosMultigenero: number
+    propagandaLGBT: number
+  }
+
+  export type DadosGraficoLinhaPontos = {
+    ano: string;
+    projetos: number;
+  }
