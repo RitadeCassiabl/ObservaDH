1233c5d1affbb1764525ce7cb3a2d4b3f2f7daf8
follijulio <follijulio@gmail.com>
2025-01-07 16:15:07 -0300

    adiciona componente CardComponte à página de teste

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,6 +1,13 @@
+import { projetosMock } from "@/lib/mock/mock_projetos";
+import CardComponte from "@/ui/cards/CardComponte";
+
 const page: React.FC = () => {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-[625rem] flex justify-center">
+      <CardComponte
+        parlamentar={projetosMock[0].parlamentares[0]}
+        propostas={14}
+      />
     </div>
   );
 };
