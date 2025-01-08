4e1d5db12a88ae01b81e4e5ad87dec08f495d5d8
follijulio <follijulio@gmail.com>
2025-01-08 00:55:56 -0300

    adiciona lógica para buscar e exibir projeto por ID, com tratamento para projeto não encontrado

--- src/app/projetos/[id]/page.tsx
+++ src/app/projetos/[id]/page.tsx
@@ -2,13 +2,25 @@
 "use client";
 
 import { useParams } from "next/navigation";
-
+import { buscarProjetoPorId } from "@/lib/utils/busca";
+import { ProjetoLei } from "@/lib/types/projetos";
 const page: React.FC = () => {
+  
   const params = useParams();
-  const id = params.id;
+  const id = Array.isArray(params.id) ? params.id[0] : params.id;
+
+  const projeto: ProjetoLei | undefined = buscarProjetoPorId(id);
+
   return (
-    <div>
-      {id}
+    <div className="h-full w-full flex justify-center">
+      <div className="bg-gradient-to-t from-[#2C52A4]/45 to-[#050B17]/45 h-5/6 w-11/12 min-h-[30rem]">
+        {projeto ? 
+        <div>
+          {projeto.pauta}
+        </div> : (
+          <p>Projeto não encontrado</p>
+        )}
+      </div>
     </div>
   );
 };
