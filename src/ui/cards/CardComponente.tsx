2f51b47d89b4eed4bcbd18aa497b60e7f628b246
follijulio <follijulio@gmail.com>
2025-01-08 16:29:57 -0300

    refatora o layout do CardComponente para melhorar a disposição dos elementos e a responsividade

--- src/ui/cards/CardComponente.tsx
+++ src/ui/cards/CardComponente.tsx
@@ -15,28 +15,32 @@ const CardComponente: React.FC<componentePros> = ({
 }) => {
   return (
     <div className={` ${oswald.className} flex flex-row w-11/12 h-28`}>
-      <section className="flex flex-row h-full w-1/2 px-16">
+      <section className="flex flex-row h-full w-1/2 ">
         <CardParlamentar parlamentar={parlamentar}>
-          <section className="flex flex-row h-full w-full gap-28 items-center imagem-hover text-white hover:text-[#93F996]">
-            <img
-              src={parlamentar.url_imagem}
-              alt=""
-              className="h-24 w-24 rounded-full object-cover"
-            />
-            <p className=" text-3xl  font-medium ">
-              {parlamentar.nome}
-            </p>
+          <section className="w-full items-center imagem-hover text-white hover:text-[#93F996] h-full px-16 grid grid-cols-2">
+            <div className="w-1/2">
+              <img
+                src={parlamentar.url_imagem}
+                alt=""
+                className="h-24 w-24 rounded-full object-cover"
+              />
+            </div>
+            <div className="w-1/2">
+              <p className=" text-3xl font-medium text-nowrap">
+                {parlamentar.nome}
+              </p>
+            </div>
           </section>
         </CardParlamentar>
       </section>
-      <div className="flex flex-row h-full w-1/2 p-7 justify-around items-center text-3xl text-white">
-        <p>
+      <div className="grid grid-cols-3 h-full w-1/2 px-16 justify-between items-center text-3xl text-white">
+        <p className="">
           {parlamentar.partido}
         </p>
-        <p>
+        <p className="">
           {parlamentar.estado}
         </p>
-        <p>
+        <p className="text-center">
           {propostas}
         </p>
       </div>
