c86b15425f3b7893bb33b83d2f3a4e8875a61f8a
follijulio <follijulio@gmail.com>
2025-01-08 10:55:10 -0300

    adiciona componente Titulo e refatora a estrutura da página para melhor semântica, utilizando <section> e <article>

--- src/app/projetos/[id]/page.tsx
+++ src/app/projetos/[id]/page.tsx
@@ -16,14 +16,30 @@ import { Oswald } from "next/font/google";
 
 const oswald = Oswald({ weight: ["400", "700"], subsets: ["latin"] });
 
+interface topicoProps {
+  titulo: string;
+  children?: React.ReactNode;
+}
+
+const Titulo: React.FC = () => {
+  return (
+    <TextContent shadow className="text-7xl">
+      <LineText>
+        <TextSmallTitillium>Dados</TextSmallTitillium>
+        <TextSpace />
+        <TextStrongOswald>da Proposta</TextStrongOswald>
+      </LineText>
+    </TextContent>
+  );
+};
+
 const page: React.FC = () => {
   const params = useParams();
+
   const id = Array.isArray(params.id) ? params.id[0] : params.id;
 
   const projeto: ProjetoLei | undefined = buscarProjetoPorId(id);
-  if (!projeto) {
-    return null;
-  }
+
   return (
     <div
       className={`h-full w-full flex flex-col items-center py-[4.5rem] gap-[4.5rem] border-[#87D9FF] ${oswald.className}`}
@@ -31,7 +47,7 @@ const page: React.FC = () => {
       <section>
         <Titulo />
       </section>
-      <article className="flex flex-col bg-gradient-to-t from-[#2C52A4]/45 to-[#050B17]/45 h-5/6 w-11/12 min-h-[30rem] py-16 px-[4.5rem] gap-12">
+      <article className="flex flex-col bg-gradient-to-t from-[#2C52A4]/45 to-[#050B17]/45 h-5/6 w-11/12 min-h-[30rem] py-16 px-[4.5rem] gap-12 border-2 border-[#87D9FF] rounded-[10px]">
         <section className="flex flex-row gap-16">
           <ItemRender
             titulo="Número"
@@ -90,15 +106,35 @@ const page: React.FC = () => {
           {projeto.ementa +
             " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste porro et asperiores, maxime tenetur dicta quis consequuntur error nulla illum ab nisi, aspernatur nesciunt veniam itaque unde sapiente libero voluptate!"}
         </Topico>
+        <Topico titulo="Justificativa">
+          {projeto.justificativa}
+        </Topico>
+        <Topico
+          titulo={
+            projeto.violacoes.length > 1
+              ? "Direitos Violados"
+              : "Direito Violado"
+          }
+        >
+          {projeto.violacoes.map((direito, index) =>
+            <p key={index} className="flex flex-col gap-2">
+              {direito}
+            </p>
+          )}
+        </Topico>
+        <Topico
+          titulo={projeto.ideologia.length > 1 ? "Ideologias" : "Ideologia"}
+        >
+          {projeto.ideologia.map((ideologia, index) =>
+            <p key={index} className="flex flex-col gap-2">
+              {ideologia}
+            </p>
+          )}
+        </Topico>
       </article>
     </div>
   );
 };
-
-interface topicoProps {
-  titulo: string;
-  children?: React.ReactNode;
-}
 
 const Topico: React.FC<topicoProps> = ({ titulo, children }) => {
   return (
@@ -107,22 +143,11 @@ const Topico: React.FC<topicoProps> = ({ titulo, children }) => {
         {titulo}
         {": "}
       </h3>
-      <p className="text-[#CDDBFF] text-3xl">
+      <div className="text-[#CDDBFF] text-3xl text-justify">
         {children}
-      </p>
+      </div>
     </section>
   );
 };
 
-const Titulo = () => {
-  return (
-    <TextContent shadow className="text-7xl">
-      <LineText>
-        <TextSmallTitillium>Dados</TextSmallTitillium>
-        <TextSpace />
-        <TextStrongOswald>da Proposta</TextStrongOswald>
-      </LineText>
-    </TextContent>
-  );
-};
 export default page;
