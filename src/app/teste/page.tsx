24e1e7556c035e30c43e6d9b67b7707cef8e8ea7
follijulio <follijulio@gmail.com>
2025-01-07 21:32:29 -0300

    remove importações e componente CardComponte da página de teste

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,13 +1,7 @@
-import { projetosMock } from "@/lib/mock/mock_projetos";
-import CardComponte from "@/ui/cards/CardComponte";
-
 const page: React.FC = () => {
   return (
     <div className="h-[625rem] flex justify-center">
-      <CardComponte
-        parlamentar={projetosMock[0].parlamentares[0]}
-        propostas={14}
-      />
+      
     </div>
   );
 };
