9927d0adbcacac564435d6fde57497a911f8f3fd
follijulio <follijulio@gmail.com>
2025-01-12 10:43:58 -0300

    adiciona dados mock para gráficos de rosquinha e barras verticais

---
+++ src/lib/mock/mock_direitos.ts
@@ -0,0 +1,17 @@
+const graficoRosquinhaDadosMock = [
+    { tema: "saude", pls: 275, fill: "#FF977A" },
+    { tema: "LIEG", pls: 200, fill: "#FDFF78" },
+    { tema: "LIB", pls: 187, fill: "#F693F9" },
+    { tema: "Educacao", pls: 173, fill: "#87D9FF"},
+  ]
+
+  const graficoBarrasVerticalDadosMock = [
+    { pauta: "Anti_Mino", pls: 65},
+    { pauta: "COAM", pls: 65},
+    { pauta: "DBR", pls: 95},
+    { pauta: "PMGF", pls: 110},
+    { pauta: "HPS", pls: 135},
+    { pauta: "Anti_Socio", pls: 145},
+    { pauta: "Imuta_Socio", pls: 150},
+  ]
+  export {graficoRosquinhaDadosMock, graficoBarrasVerticalDadosMock}
