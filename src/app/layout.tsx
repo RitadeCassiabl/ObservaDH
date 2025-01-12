c321a46de652507942e94061eb134671825132c7
follijulio <follijulio@gmail.com>
2025-01-11 23:04:33 -0300

    remove padding do contêiner principal no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           <NavBar />
         </header>
         <main className="h-full w-full bg-grid">
-          <div className="h-full w-full px-11 py-16">
+          <div className="h-full w-full">
             {children}
           </div>
         </main>
