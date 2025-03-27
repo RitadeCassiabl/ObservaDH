b378722659e3e57a7476f1ea8624d9360a5da6ac
follijulio <follijulio@gmail.com>
2025-03-27 00:37:24 -0300

    atualizaa layout e estiliza o FooterBar com novos elementos e responsividade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,16 +15,16 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-br" className={titillium_web.className}>
-      <body className="bg-layout-principal antialiased no-scrollbar">
+      <body className="bg-layout-principal antialiased no-scrollbar flex flex-col">
         <header className="h-[34rem] w-full">
           <NavBar />
         </header>
         <main className="h-full w-full bg-grid">
-          <div className="h-full w-full">
+          <div className="h-full w-full my-10">
             {children}
           </div>
         </main>
-        <footer className="h-72 w-full">
+        <footer className="h-[18.625rem] w-full">
           <FooterBar />
         </footer>
       </body>
