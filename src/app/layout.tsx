ea0cfd18b1cca7c61b8ba2ea0c527250adf3bdd6
= <=>
2025-04-02 22:24:36 -0300

    Reestrutura layout principal e adiciona componentes NavBar e FooterBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { titillium_web } from "@/ui/fonts";
-import NavBar from "../ui/components/NavBar";
-import FooterBar from "../ui/components/FooterBar";
-
 
 export const metadata: Metadata = {
     title: "ObservaDH",
@@ -15,18 +12,8 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-br" className={titillium_web.className}>
-      <body className="bg-layout-principal antialiased no-scrollbar flex flex-col">
-        <header className="h-[34rem] w-full">
-          <NavBar />
-        </header>
-        <main className="h-full w-full bg-grid">
-          <div className="h-full w-full my-10">
-            {children}
-          </div>
-        </main>
-        <footer className="h-[18.625rem] w-full">
-          <FooterBar />
-        </footer>
+      <body>
+     {children}
       </body>
     </html>
   );
