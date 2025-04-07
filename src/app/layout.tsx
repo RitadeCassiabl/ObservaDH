b3c3d0a604aafbfcd37df26eb9d77025428ddcd8
follijulio <follijulio@gmail.com>
2025-04-07 14:35:12 -0300

    Revert "atualizaa layout e estiliza o FooterBar com novos elementos e responsividade"
    This reverts commit b378722659e3e57a7476f1ea8624d9360a5da6ac.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,18 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-br" className={titillium_web.className}>
-      <body>
-     {children}
+      <body className="bg-layout-principal antialiased no-scrollbar flex flex-col">
+        <header className="h-[34rem] w-full">
+          <NavBar />
+        </header>
+        <main className="h-full w-full bg-grid">
+          <div className="h-full w-full my-10">
+            {children}
+          </div>
+        </main>
+        <footer className="h-[18.625rem] w-full">
+          <FooterBar />
+        </footer>
       </body>
     </html>
   );
