afb0a12243343046ad83b627d981c074230e846e
follijulio <follijulio@gmail.com>
2025-04-22 10:32:49 -0300

    refatoração: ajustar formatação e restrições de chave estrangeira na migração do banco de dados

--- prisma/migrations/20250421143803_add_unique_to_direito_violado_nome/migration.sql
+++ prisma/migrations/20250421143803_add_unique_to_direito_violado_nome/migration.sql
@@ -1,24 +1,24 @@
 /*
-  Warnings:
+Warnings:
 
-  - A unique constraint covering the columns `[nome]` on the table `DireitoViolado` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[nome]` on the table `Profissao` will be added. If there are existing duplicate values, this will fail.
+- A unique constraint covering the columns `[nome]` on the table `DireitoViolado` will be added. If there are existing duplicate values, this will fail.
+- A unique constraint covering the columns `[nome]` on the table `Profissao` will be added. If there are existing duplicate values, this will fail.
 
-*/
+ */
 -- CreateIndex
-CREATE UNIQUE INDEX "DireitoViolado_nome_key" ON "DireitoViolado"("nome");
+CREATE UNIQUE INDEX "DireitoViolado_nome_key" ON "DireitoViolado" ("nome");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Profissao_nome_key" ON "Profissao"("nome");
+CREATE UNIQUE INDEX "Profissao_nome_key" ON "Profissao" ("nome");
 
 -- AddForeignKey
-ALTER TABLE "Projeto" ADD CONSTRAINT "Projeto_ambitoId_fkey" FOREIGN KEY ("ambitoId") REFERENCES "Ambito"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Projeto" ADD CONSTRAINT "Projeto_ambitoId_fkey" FOREIGN KEY ("ambitoId") REFERENCES "Ambito" ("id") ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Projeto" ADD CONSTRAINT "Projeto_temaId_fkey" FOREIGN KEY ("temaId") REFERENCES "Tema"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Projeto" ADD CONSTRAINT "Projeto_temaId_fkey" FOREIGN KEY ("temaId") REFERENCES "Tema" ("id") ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Politico" ADD CONSTRAINT "Politico_estadoId_fkey" FOREIGN KEY ("estadoId") REFERENCES "Estado"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Politico" ADD CONSTRAINT "Politico_estadoId_fkey" FOREIGN KEY ("estadoId") REFERENCES "Estado" ("id") ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Politico" ADD CONSTRAINT "Politico_partidoId_fkey" FOREIGN KEY ("partidoId") REFERENCES "Partido"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Politico" ADD CONSTRAINT "Politico_partidoId_fkey" FOREIGN KEY ("partidoId") REFERENCES "Partido" ("id") ON DELETE NO ACTION ON UPDATE CASCADE;
