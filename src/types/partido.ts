878dbce58c49776f9278f7878fbd27738453201e
follijulio <follijulio@gmail.com>
2025-04-23 14:08:28 -0300

    refatoração: renomear tipo 'Partido' para 'PartidoModel' e adicionar classe 'Partido' com propriedades e construtor

--- src/types/partido.ts
+++ src/types/partido.ts
@@ -1,7 +1,33 @@
-export type Partido = {
+export type PartidoModel = {
     nome: string;
     sigla: string;
     propostas: string;
     url_imagem: string;
     parlamentares: string;
 };
+
+export class Partido {
+    id?: string;
+    nome: string;
+    sigla: string;
+    imagem: string;
+    projetos?: string[];
+    politicos?: string[];
+
+    constructor(
+        nome: string,
+        sigla: string,
+        imagem: string,
+        id?: string,
+        projetos?: string[],
+        politicos?: string[]
+    ) {
+        this.nome = nome;
+        this.sigla = sigla;
+        this.imagem = imagem;
+        this.id = id;
+        this.projetos = projetos || [];
+        this.politicos = politicos || [];
+    }
+
+}
