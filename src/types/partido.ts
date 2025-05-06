2e606dcc9ee5edeb89f6a158a01590edae7224d4
follijulio <follijulio@gmail.com>
2025-05-05 21:24:00 -0300

    refactor: transformar 'Partido' de tipo para classe e atualizar propriedades

--- src/types/partido.ts
+++ src/types/partido.ts
@@ -1,7 +1,23 @@
-export type Partido = {
+class Partido {
+    id?: number;
     nome: string;
-    sigla: string;
-    propostas: string;
-    url_imagem: string;
-    parlamentares: string;
-};
+    codigo: string;
+    projetos?: string[];
+    politicos?: string[];
+
+    constructor(
+        nome: string,
+        codigo: string,
+        politicos?: string[],
+        id?: number,
+        projetos?: string[]
+    ) {
+        this.id = id;
+        this.nome = nome;
+        this.codigo = codigo;
+        this.politicos = politicos;
+        this.projetos = projetos;
+    }
+}
+
+export { Partido };
