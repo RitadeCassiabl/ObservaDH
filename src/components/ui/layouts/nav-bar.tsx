f8137f4176986ce268b9e6167fb71bfed7665f70
follijulio <follijulio@gmail.com>
2025-05-15 22:54:44 -0300

    feat: adicionar página 404 personalizada com fundo e cabeçalho

--- src/components/ui/layouts/nav-bar.tsx
+++ src/components/ui/layouts/nav-bar.tsx
@@ -2,12 +2,11 @@
 
 "use client";
 
-import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
 
-import clsx from "clsx";
 import useDescobertaCabecalho from "@/lib/utils/cabecalho-utils";
+import Header from "./Header";
 
 interface Cabecalho {
   titulo: string;
@@ -41,41 +40,7 @@ const navBar: React.FC = () => {
   return (
     <div className="w-full h-full flex flex-col items-center bg-senado bg-cover bg-center border-b-2 border-[#001745]">
       <nav className="p-8 w-full items-center flex flex-col gap-40">
-        <div className="flex w-full items-center justify-center flex-col">
-          <div className="w-11/12 flex flex-col">
-            <div className="flex gap-[12.5rem] items-center justify-between">
-              <Link href={"/"} className="z-10">
-                <h1 className="text-[3.125rem] text-white logo" />
-              </Link>
-              <ul className="flex text-3xl font-normal text-white gap-[3.125rem] font">
-                {[
-                  { titulo: "PLs", rota: "/projetos" },
-                  { titulo: "Parlamentares", rota: "/parlamentares" },
-                  { titulo: "Direitos", rota: "/direitos" },
-                  { titulo: "Sobre", rota: "/sobre" },
-                  { titulo: "Desenvolvedores", rota: "/desenvolvedores" },
-                ].map((item) => {
-                  return (
-                    <li key={item.titulo} className="z-10">
-                      <Link
-                        href={item.rota}
-                        className={clsx(
-                          "flex items-center justify-center hover:text-[#4568BE]",
-                          {
-                            "text-[#4568BE]": router === item.rota,
-                          }
-                        )}
-                      >
-                        {item.titulo}
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-          <span className="border-b-[1px] shadow-bottom shadow-white w-full" />
-        </div>
+        <Header />
         <div className="w-full flex flex-col px-14">
           {router === "/" ? (
             <div className="gap-8 flex flex-col">
