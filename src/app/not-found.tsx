1175324e9d859cc311ae802e597622b8b7f5a6fd
follijulio <follijulio@gmail.com>
2025-05-17 01:12:38 -0300

    refactor: ajustar layout e espaçamento na página 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -16,17 +16,15 @@ const page: React.FC = () => {
 	return (
 		<div className="fundo-404 overflow-auto no-scrollbar flex flex-col gap-16">
 			<div
-				className={`flex flex-col p-8 gap-20 text-white text-shadow-xl ${oswald.className}`}
+				className={`flex flex-col p-8 gap-40 text-white text-shadow-xl ${oswald.className}`}
 			>
-				<header className="flex flex-col gap-32">
-					<Header />
-					<h2 className={`text-9xl  `}>{"Perdido no espaço :("}</h2>
-				</header>
-				<div className="flex flex-col gap-16">
-					<p className="text-8xl">{"404 - Página não Encontrada"}</p>
+				<Header />
+				<div className="flex flex-col gap-16 pl-20">
+					<h2 className={`text-8xl`}>{"Perdido no espaço :("}</h2>
+					<p className="text-7xl">{"404 - Página não Encontrada"}</p>
 					<button
 						onClick={retornar}
-						className="rounded-full bg-[#2C52A4] border-[#87D9FF] border-2 h-20 w-72 text-center text-[#91ADF4] text-3xl"
+						className="rounded-[5px] bg-[#2C52A4] border-[#87D9FF] border-2 h-12 w-48 text-center text-[#91ADF4] text-xl"
 					>
 						{"Voltar a página inicial"}
 					</button>
