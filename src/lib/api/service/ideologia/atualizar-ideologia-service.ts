0c91248f9b872fa648b76a1fc9ff79d1309d922a
follijulio <follijulio@gmail.com>
2025-05-19 02:38:59 -0300

    colaterais - atualizações models

--- src/lib/api/service/ideologia/atualizar-ideologia-service.ts
+++ src/lib/api/service/ideologia/atualizar-ideologia-service.ts
@@ -1,14 +1,17 @@
+import { Ideologia } from "@/domain/models/ideologia";
 import { prismaClient } from "@/services/prisma/prisma";
 export class AtualizarIdeologiaService {
-	async executar(id: string, nome: string) {
+	async executar({ ideologia }: { ideologia: Ideologia }) {
 		const prisma = prismaClient;
 
 		const resposta = await prisma.ideologia.update({
 			where: {
-				id: id,
+				id: ideologia.id,
 			},
 			data: {
-				nome: nome,
+				nome: ideologia.nome,
+				descricao: ideologia.descricao,
+				sigla: ideologia.sigla,
 			},
 		});
 		return resposta;
